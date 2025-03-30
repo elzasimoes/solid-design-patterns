ca72a651376f2a805adee7516ef11a73e2b607ae
elza <elzaesimoes@gmail.com>
2025-03-30 11:36:19 -0400

    update 1 file and create 1 file

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from item import Item
 from pedido.pedido_retirada import PedidoRetirada
 from pedido.pedido_delivery import PedidoDelivery
 
+from pagamento.pagamento_factory import PagamentoFactory
+
+
 cliente = Cliente("Laís", "Alura")
 item_um = Item("Pizza", 30.0)
 item_dois = Item("Refrigerante", 5.0)
@@ -17,6 +20,10 @@ print(f"Preço do Pedido Delivery: {pedido_delivery.calcular_total():.2f}")
 
 
 valor_pedido = pedido_delivery.calcular_total()
-pagamento_cartao = PagamentoCartao().processar(valor_pedido)
-pagamento_pix = PagamentoPIX()
-pagamento_pix.processar(valor_pedido)
+#pagamento_cartao = PagamentoCartao().processar(valor_pedido)
+#pagamento_pix = PagamentoPIX()
+#pagamento_pix.processar(valor_pedido)
+
+tipo_pagamento = "pix"
+pagamento = PagamentoFactory.criar_pagamento(tipo_pagamento)
+pagamento.processar(valor_pedido)
